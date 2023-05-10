99357c4cddc744578df0b226d00704dc40491dc1
Young-Chan Park <young.chan.park93@gmail.com>
2023-05-10 14:03:55 +0200

    Sign-up page's country selection is now determined by the entries in the  table in the database

--- src/main/html/webapp/components/core/user/signup/signup.js
+++ src/main/html/webapp/components/core/user/signup/signup.js
@@ -2,26 +2,86 @@ import Control from 'can-control';
 import $ from 'jquery';
 
 import User from 'models/user';
-// import Country from 'models/country';
+import Country from 'models/country-signup';
 
 import template from './signup.stache';
-
+import "./signup.css";
 
 export default Control.extend({
-
   "init": function(element, options) {
-    $(element).hide();
-    $(element).html(template());
-    $(element).fadeIn();
+    var params = {};
+    Country.findAll(
+      params,
+      function(countries) {
+        $(element).html(template({
+          countries: countries
+        }));
+        $(element).fadeIn();
+      },
+      function(response) {
+        new ErrorPage(element, response);
+      });
 
-    $("#terms-and-conditions").scroll(function() {
-      var scrollTop = $(this).scrollTop(); // $("#terms-and-conditions").scrollTop()
-      var tcHeight = $(this).height(); // $("#terms-and-conditions").height()
-      var scrolled = Math.ceil(scrollTop + tcHeight);
-      if (scrolled >= $(this)[0].scrollHeight) {
-        $('#accept-terms-and-conditions')[0].disabled = false
-      }
+    // $(element).hide();
+    // $(element).html(template());
+    // $(element).fadeIn();
+
+    // $(document).on("scroll", "#terms-and-conditions .tac-background .content", function() {
+    //   var scrollTop = $(this).scrollTop();
+    //   var tcHeight = $(this).height();
+    //   var scrolled = Math.ceil(scrollTop + tcHeight);
+    //   var scrollHeight = $(this)[0].scrollHeight;
+    //   console.log(scrolled);
+    //   console.log(scrollHeight);
+    //   if (scrolled >= scrollHeight) {
+    //     $('#accept-terms-and-conditions')[0].disabled = false
+    //   }
+    // });
+
+    $(document).on("click", "#tos-view-btn", function () {
+      $("#terms-and-conditions").removeClass("hidden");
     });
+
+    $(document).on("click", ".close-btn", function () {
+      $("#terms-and-conditions").addClass("hidden");
+    });
+
+    // setTimeout(function() {
+    //   var contentElement = document.querySelector("#terms-and-conditions .tac-background .content");
+    //   console.log(contentElement); // Debugging statement
+    //   contentElement.addEventListener("scroll", function() {
+    //     var scrollTop = this.scrollTop;
+    //     var tcHeight = this.clientHeight;
+    //     var scrolled = Math.ceil(scrollTop + tcHeight);
+    //     var scrollHeight = this.scrollHeight;
+    //     if (scrolled >= scrollHeight) {
+    //       document.querySelector('#accept-terms-and-conditions').disabled = false;
+    //       document.querySelector('#accept-eu').disabled = false;
+    //     }
+    //   });
+    // }, 1000);
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          var contentElement = document.querySelector("#terms-and-conditions .tac-background .content");
+          if (contentElement) {
+            contentElement.addEventListener("scroll", function() {
+              var scrollTop = this.scrollTop;
+              var tcHeight = this.clientHeight;
+              var scrolled = Math.ceil(scrollTop + tcHeight);
+              var scrollHeight = this.scrollHeight;
+              console.log(scrolled)
+              if (scrolled >= scrollHeight) {
+                document.querySelector('#accept-terms-and-conditions').disabled = false;
+                document.querySelector('#accept-eu').disabled = false;
+              }
+            });
+            observer.disconnect();
+          }
+        }
+      });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
   },
 
   'submit': function(element, event) {
@@ -51,9 +111,29 @@ export default Control.extend({
     var passwordError = user.checkPassword(newPassword.val(), confirmNewPassword.val());
     this.updateControl(newPassword, passwordError);
 
+    // institute email
+    var instituteName = $(element).find("[name='institute-mail']")
+    var instituteNameError = (instituteName.val() !== "" ? undefined : 'Must input the email of your institute supervisor/legal-representative')
+    this.updateControl(instituteName, instituteNameError);
+
     // institute name
     var instituteName = $(element).find("[name='institute-name']")
     var instituteNameError = (instituteName.val() !== "" ? undefined : 'Must input your institute name')
+    this.updateControl(instituteName, instituteNameError);
+
+    // institute address
+    var instituteName = $(element).find("[name='institute-address']")
+    var instituteNameError = (instituteName.val() !== "" ? undefined : 'Must input your institute address')
+    this.updateControl(instituteName, instituteNameError);
+
+    // institute city
+    var instituteName = $(element).find("[name='institute-city']")
+    var instituteNameError = (instituteName.val() !== "" ? undefined : 'Must input your institute city')
+    this.updateControl(instituteName, instituteNameError);
+
+    // institute postal code
+    var instituteName = $(element).find("[name='institute-postcode']")
+    var instituteNameError = (instituteName.val() !== "" ? undefined : 'Must input your institute postal code')
     this.updateControl(instituteName, instituteNameError);
 
     // institute country
@@ -64,6 +144,11 @@ export default Control.extend({
     // terms & conditions
     var termsAndConditions = $(element).find("[name='accept-terms-and-conditions']") // document.querySelector('#accept-terms-and-conditions').checked;
     var termsAndConditionsError = (termsAndConditions[0].checked ? undefined : 'Must accept the terms & conditions')
+    this.updateControl(termsAndConditions, termsAndConditionsError);
+
+    // EU-/EAA-country
+    var termsAndConditions = $(element).find("[name='accept-eu']")
+    var termsAndConditionsError = (termsAndConditions[0].checked ? undefined : 'Must agree to only use the service within the EU-/EEA-country')
     this.updateControl(termsAndConditions, termsAndConditionsError);
 
     if (usernameError || fullnameError || mailError || passwordError || termsAndConditionsError || instituteNameError || instituteCountryError) {
---
+++ src/main/java/cloudgene/mapred/api/v2/users/GetAllowedCountries.java
@@ -0,0 +1,40 @@
+package cloudgene.mapred.api.v2.users;
+
+import java.util.List;
+
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
+
+import cloudgene.mapred.util.BaseResource;
+
+import cloudgene.mapred.core.Country;
+import cloudgene.mapred.database.CountryDao;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+
+public class GetAllowedCountries extends BaseResource {
+    
+    @Get
+	public Representation get() {
+
+		CountryDao dao = new CountryDao(getDatabase());
+
+		List<Country> countries = dao.findAll();
+
+
+        JSONArray jsonArray = new JSONArray();
+        for (Country country : countries) {
+            if (country.getAllowed() == true && country.getDisplay() == true) {
+                JSONObject object = new JSONObject();
+                object.put("name", country.getName());
+                jsonArray.add(object);
+            }
+        }
+        JSONObject object = new JSONObject();
+		object.put("data", jsonArray);
+
+		return new StringRepresentation(object.toString());
+    };
+}
