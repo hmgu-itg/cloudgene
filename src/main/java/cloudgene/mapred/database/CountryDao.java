3802bebda97d091eb5ba753672fea6bd7e43ea0a
Young-Chan Park <young.chan.park93@gmail.com>
2024-01-18 14:05:01 +0100

    Removed old genepi-db dependencies in CountryDao.java

--- src/main/java/cloudgene/mapred/database/CountryDao.java
+++ src/main/java/cloudgene/mapred/database/CountryDao.java
@@ -9,9 +9,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import cloudgene.mapred.core.Country;
-import genepi.db.Database;
-import genepi.db.IRowMapper;
-import genepi.db.JdbcDataAccessObject;
+import cloudgene.mapred.database.util.Database;
+import cloudgene.mapred.database.util.IRowMapper;
+import cloudgene.mapred.database.util.JdbcDataAccessObject;
 
 public class CountryDao extends JdbcDataAccessObject {
 
