8310500897d4d98550cdf137646d3ab9296ea1a5
andrei-barysenka <andrei.barysenka@gmail.com>
2024-06-09 23:39:25 +0200

    use lines, add metrics selector

--- src/main/html/webapp/components/admin/hadoop/hadoop.js
+++ src/main/html/webapp/components/admin/hadoop/hadoop.js
@@ -7,67 +7,97 @@ import Counter from 'models/counter';
 import template from './hadoop.stache';
 
 export default Control.extend({
+    "init": function(element, options) {
+	$(element).hide();
+	var that = this;
+	window.Raphael = Raphael;
 
-  "init": function(element, options) {
+	Counter.findOne({}, function(counter) {
+	    $(element).html(template({
+		counter: counter
+	    }));
+	    $(element).fadeIn();
 
-    $(element).hide();
-    var that = this;
+	    $.getJSON("/api/v2/admin/server/hadoop", {
+		day: ""
+	    }, function(mydata) {
+		// all distinct dates		
+		var dates=new Array();
+		for (const x of mydata){
+		    var s=x.T.split(" ")[0];
+		    if (! dates.includes(s)){
+			dates.push(s);
+		    }
+		}
+		
+		var select = document.getElementById('day_combo');
+		for (const x of dates){
+		    var opt = document.createElement('option');
+		    opt.value = x;
+		    opt.innerHTML = x;
+		    select.appendChild(opt);
+		    //console.log(x);
+		}
+		
+		var metrics = document.getElementById('metrics_combo');
+		for (const x of Object.keys(mydata[0]).sort()){
+		    if (x=="T" || x.includes("CURSTATE"))
+			continue;
+		    var opt = document.createElement('option');
+		    opt.value = x;
+		    opt.innerHTML = x;
+		    metrics.appendChild(opt);
+		}
 
-    window.Raphael = Raphael;
+		var day = $("#day_combo").val();
+		var m = $("#metrics_combo").val();
+		that.options.running = Morris.Line({
+		    element: 'morris-area-chart',
+		    data: mydata.filter(function(x){return x.T.split(" ")[0]==day;}),
+		    xkey: 'T',
+		    ykeys: [m],
+		    labels: [m],
+		    pointSize: 0,
+		    hideHover: 'always',
+		    smooth: 'false',
+		    resize: true
+		});
+	    });
+	}, function(message) {
+	});
+    },
 
-    Counter.findOne({}, function(counter) {
-      $(element).html(template({
-        counter: counter
-      }));
-      $(element).fadeIn();
-
+    '#day_combo change': function() {
+	var day = $("#day_combo").val();
+	var that = this;
 	$.getJSON("/api/v2/admin/server/hadoop", {
-        day: ""
-      }, function(mydata) {
+	    day: day
+	}, function(mydata) {
+	    that.options.running.setData(mydata);
+	});
+    },
 
-        $("#new_users").html(mydata[0].users - mydata[mydata.length - 1].users);
-        $("#total_users").html(mydata[0].users);
-
-        $("#new_jobs").html(mydata[0].completeJobs - mydata[mydata.length - 1].completeJobs);
-        $("#total_jobs").html(mydata[0].completeJobs);
-
-        that.options.running = Morris.Area({
-          element: 'morris-area-chart',
-          data: mydata,
-          xkey: 'timestamp',
-          ykeys: ['runningJobs', 'waitingJobs'],
-          labels: ['Running Jobs', 'Waiting Jobs'],
-          pointSize: 0,
-          hideHover: 'always',
-          smooth: 'false',
-          resize: true
-        });
-
-
-      });
-    }, function(message) {
-
-    });
-  },
-
-  '#day_combo change': function() {
-
-    var days = $("#day_combo").val();
-    var that = this;
-    $.getJSON("/api/v2/admin/server/hadoop", {
-      day: day
-    }, function(mydata) {
-
-      $("#new_users").html(mydata[0].users - mydata[mydata.length - 1].users);
-      $("#total_users").html(mydata[0].users);
-
-      $("#new_jobs").html(mydata[0].completeJobs - mydata[mydata.length - 1].completeJobs);
-      $("#total_jobs").html(mydata[0].completeJobs);
-
-      that.options.running.setData(mydata);
-      //that.options.jobs.setData(mydata);
-      //that.options.users.setData(mydata);
-    });
-  }
+    '#metrics_combo change': function() {
+	var metrics = $("#metrics_combo").val();
+	var day = $("#day_combo").val();
+	var that = this;
+	console.log("day: "+day+" metrics: "+metrics);
+	$("#morris-area-chart").html("");
+	$.getJSON("/api/v2/admin/server/hadoop", {
+	    day: day
+	}, function(mydata) {
+	    that.options.running = Morris.Line({
+		element: 'morris-area-chart',
+		data: mydata,
+		xkey: 'T',
+		ykeys: [metrics],
+		labels: [metrics],
+		pointSize: 0,
+		hideHover: 'always',
+		smooth: 'false',
+		resize: true
+	    });
+	});
+    }
 
 });
