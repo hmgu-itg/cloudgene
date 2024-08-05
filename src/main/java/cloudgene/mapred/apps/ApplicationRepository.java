a87a8277d851671dae0f458125955d9010d70289
Young-Chan Park <young.chan.park93@gmail.com>
2024-08-05 14:23:13 +0200

    Actually implement ApplicationRepository not returning apps if user is blocked

--- src/main/java/cloudgene/mapred/apps/ApplicationRepository.java
+++ src/main/java/cloudgene/mapred/apps/ApplicationRepository.java
@@ -85,8 +85,7 @@ public class ApplicationRepository {
 		Application app = getById(id);
 
 		if (app != null && app.isEnabled() && app.isLoaded() && !app.hasSyntaxError()) {
-			// if (user == null || user.hasRole("block")) {
-			if (user == null) {
+			if (user == null || user.hasRole("block")) {
 				if (app.hasPermission(("public"))) {
 					if (app.getWdlApp().getWorkflow() != null) {
 						return app;
