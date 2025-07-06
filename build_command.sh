7fbc05cbeb6ed4abdcc6669fcb10da4cad1daa92
andrei-barysenka <andrei.barysenka@gmail.com>
2025-07-06 15:47:59 +0200

    make build_command executable shell script

---
+++ build_command.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+mvn -Dmaven.test.skip=true package
+
+chmod +x ./target/cloudgene-2.8.7-assembly/cloudgene
+
+rm -rf ~/cloudgene_exec/webapp/
+cp -r ./target/cloudgene-2.8.7-assembly/webapp/ ~/cloudgene_exec/
+
+rm -rf ~/cloudgene_exec/sample
+cp -r ./target/cloudgene-2.8.7-assembly/sample ~/cloudgene_exec/
+
+rm -rf ~/cloudgene_exec/lib
+cp -r ./target/cloudgene-2.8.7-assembly/lib ~/cloudgene_exec/
+
+rm ~/cloudgene_exec/cloudgene
+cp ./target/cloudgene-2.8.7-assembly/cloudgene ~/cloudgene_exec/
+
+rm ~/cloudgene_exec/cloudgene.jar
+cp ./target/cloudgene-2.8.7-assembly/cloudgene.jar ~/cloudgene_exec/
+
+exit 0
