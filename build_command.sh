7215f7220d51e729e58c9c19958c35b324d009a1
andrei-barysenka <andrei.barysenka@gmail.com>
2025-08-09 16:06:25 +0200

    add verbosity

--- build_command.sh
+++ build_command.sh
@@ -4,19 +4,19 @@ mvn install -Dmaven.test.skip=true
 
 chmod +x ./target/cloudgene-2.8.7-assembly/cloudgene
 
-rm -rf ~/cloudgene_exec/webapp/
-cp -r ./target/cloudgene-2.8.7-assembly/webapp/ ~/cloudgene_exec/
+rm -rvf ~/cloudgene_exec/webapp/
+cp -rv ./target/cloudgene-2.8.7-assembly/webapp/ ~/cloudgene_exec/
 
-rm -rf ~/cloudgene_exec/sample
-cp -r ./target/cloudgene-2.8.7-assembly/sample ~/cloudgene_exec/
+rm -rvf ~/cloudgene_exec/sample
+cp -rv ./target/cloudgene-2.8.7-assembly/sample ~/cloudgene_exec/
 
-rm -rf ~/cloudgene_exec/lib
-cp -r ./target/cloudgene-2.8.7-assembly/lib ~/cloudgene_exec/
+rm -rvf ~/cloudgene_exec/lib
+cp -rv ./target/cloudgene-2.8.7-assembly/lib ~/cloudgene_exec/
 
-rm ~/cloudgene_exec/cloudgene
-cp ./target/cloudgene-2.8.7-assembly/cloudgene ~/cloudgene_exec/
+rm -v ~/cloudgene_exec/cloudgene
+cp -v ./target/cloudgene-2.8.7-assembly/cloudgene ~/cloudgene_exec/
 
-rm ~/cloudgene_exec/cloudgene.jar
-cp ./target/cloudgene-2.8.7-assembly/cloudgene.jar ~/cloudgene_exec/
+rm -v ~/cloudgene_exec/cloudgene.jar
+cp -v ./target/cloudgene-2.8.7-assembly/cloudgene.jar ~/cloudgene_exec/
 
 exit 0
