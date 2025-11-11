f7c4dfe128411f81cc72ae2fd20671b1eb00de64
andrei-barysenka <andrei.barysenka@gmail.com>
2025-11-11 11:01:18 +0100

    add logging

--- src/main/html/webapp/components/core/user/signup/signup.js
+++ src/main/html/webapp/components/core/user/signup/signup.js
@@ -108,10 +108,13 @@ export default Control.extend({
     // institute email
     var instituteEmail = $(element).find("[name='institute-mail']")
       var instituteEmailError0 = user.checkMail(instituteEmail.val());
-      console.log("email: "+instituteEmail.val());
-      console.log("error0: "+instituteEmailError0);
     var instituteEmailError = (instituteEmail.val() !== "" ? undefined : 'Must input the email of your institute supervisor/legal-representative')
       var instituteEmailError1 = (instituteEmail.val() !== mail.val() ? undefined : 'Email of your institute supervisor/legal-representative must be different from your own')
+      console.log("email: "+mail.val());
+      console.log("institute email: "+instituteEmail.val());
+      console.log("error0: "+instituteEmailError0);
+      console.log("error1: "+instituteEmailError1);
+      console.log("error: "+instituteEmailError);
       if (instituteEmailError0 !== undefined){
 	  this.updateControl(instituteEmail,instituteEmailError0);
       }
